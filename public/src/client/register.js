70e1425134199940b6047274feaf6e8a6133ba53
margarethe913 <73721525+margarethe913@users.noreply.github.com>
2024-09-16 11:42:57 -0400

    Adding username suggestion to registration form

--- public/src/client/register.js
+++ public/src/client/register.js
@@ -131,7 +131,8 @@ define('forum/register', [
                 if (results.every(obj => obj.status === 'rejected')) {
                     showSuccess(username_notify, successIcon);
                 } else {
-                    showError(username_notify, '[[error:username-taken]]');
+                    const msg = '[[error:username-taken]]. Maybe try "' + username.concat("suffix") + '"';
+                    showError(username_notify, msg);
                 }
 
                 callback();
